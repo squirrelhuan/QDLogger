df07c8d8caaf1d46af222399c4442d1abe020238
huan <978252909@qq.com>
2021-07-17 23:26:37 +0800

    commit 4

---
+++ qdlogger_library/src/main/java/cn/demomaster/qdlogger_library/format/MapKeyComparator.java
@@ -0,0 +1,13 @@
+package cn.demomaster.qdlogger_library.format;
+
+import java.util.Comparator;
+
+public class MapKeyComparator implements Comparator<Integer> {
+    // 返回值为int类型。
+    // 大于0表示顺序（递增），小于0表示逆序（递减）。
+    // 0，相等。
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1>o2?1:-1;
+    }
+}
